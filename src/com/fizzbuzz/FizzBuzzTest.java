123e290e348c63628eb51a59118353c25b0f369d
Motchell <hidalgomora.ma@gmail.com>
2024-04-05 03:03:26 -0300

    First commit for FizzBuzz

---
+++ src/com/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,13 @@
+package com.fizzbuzz;
+
+public class FizzBuzzTest {
+
+	public static void main(String[] args) {
+		FizzBuzz FzzBzz = new FizzBuzz();
+		System.out.println(FzzBzz.fizzBuzz(3));
+		System.out.println(FzzBzz.fizzBuzz(5));
+		System.out.println(FzzBzz.fizzBuzz(15));
+		System.out.println(FzzBzz.fizzBuzz(2));
+	}
+	// MITCHELL HIDALGO - TODOS LOS DERECHOS RESERVADOS EJEJEJE
+}
